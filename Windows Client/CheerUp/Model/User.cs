580e6955219721039c282cf0efe4cb3b600ce474
KimJunYoung <sunion10@naver.com>
2018-11-10 17:41:34 +0900

    [Add]
    서버와 통신하기 위한 기초 코드 작성
    - 모델 생성(유저, 장소, 메세지)
    - Json을 쉽게 이용하기 위한 Nuget 설치(NewtomSoft.Json)

---
+++ Windows Client/CheerUp/Model/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheerUp.Model
+{
+    public class User : INotifyPropertyChanged
+    {
+        private int idx;
+        public int Idx
+        {
+            get
+            {
+                return idx;
+            }
+            set
+            {
+                idx = value;
+                NotifyPropertyChanged(nameof(Idx));
+            }
+        }
+
+        private string id;
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                NotifyPropertyChanged(nameof(Id));
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                NotifyPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
