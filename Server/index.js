a5f38e3bfd19f214b0a85f20141d05c2e6155c61
tbvjaos510 <tbvjaos510@naver.com>
2018-11-10 20:56:22 +0900

    [CHANGE] express 추가

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,17 @@
-const io = require('socket.io')(80)
+var express = require('express');
+const http = require('http')
+var app = express();
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        next();
+    });
+var server = http.createServer(app);
+
+const io = require('socket.io').listen(server)
 io.set('origins', '*:*')
 io.on('connection', require('./socket'))
+
+server.listen(80)
